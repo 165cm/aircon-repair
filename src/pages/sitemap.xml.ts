8d2bff8c6210cf8145ce58526116df3888d0e2e8
Papazon <1650mm@gmail.com>
2026-05-17 07:38:23 +0900

    🚀 Publish src/pages/sitemap.xml.ts

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,30 @@
+import { getCollection } from "astro:content";
+import { affiliate } from "@data/affiliate";
+import { productCategories } from "@data/products";
+
+export async function GET() {
+  const articles = await getCollection("articles");
+  const staticPaths = [
+    "/",
+    "/diagnosis/",
+    "/by-size/",
+    "/basics/",
+    "/cleaning-prevention/",
+    "/products/",
+    "/jobs/",
+    "/disclaimer/",
+    "/articles/",
+    "/about/"
+  ];
+  const articlePaths = articles.map((article) => `/articles/${article.slug}/`);
+  const productPaths = productCategories.map((product) => `/products/${product.id}/`);
+  const urls = [...staticPaths, ...articlePaths, ...productPaths]
+    .map((path) => `<url><loc>${new URL(path, affiliate.siteUrl).toString()}</loc></url>`)
+    .join("");
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`, {
+    headers: {
+      "Content-Type": "application/xml"
+    }
+  });
+}
